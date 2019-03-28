f76f1cc1f3fea4f4feb960926af77eae5815ea8b
AMoPRo <amjoshaghani@gmail.com>
2019-03-28 14:16:51 +0430

    amopro

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ColorPicker
+this script help you for find color you want and get its **RGB** and **Hash**.
+[![AMoPRo](https://chor.ir/api/file/image/BQADBAADEgYAAn1TyVB3IDxKlNRrogI.png "AMoPRo")](http://amopro.ir "AMoPRo")
+
+## how to use it?
+- if your os is from debian, you can open `Linux` directory from `executables` directory.
+
+- else if your os is Windows, you can open `Windows` folder from `executables` folder.
+
+- else, you can open `bin` directory and execute .py file from it. how?? well, install `Python` for your os and open **pythonFile** and  terminal will open and you can execute it from [pyInstaller](https://www.pyinstaller.org/ "pyInstaller") framework.
+
+------------
+
+**Note: python files are in bin folder**
+### witch frame work used?
+**tkinter**
+
+>God bless who bless peoples
+***Amopro***
---
+++ bin/ColorPicker.py
@@ -0,0 +1,25 @@
+# in the name of god merciful | به نام خداوند بخشنده مهربان
+from tkinter import *
+from tkinter.colorchooser import *
+# ---------------------------------
+root = Tk()
+root.title("Color Picker")
+root.iconbitmap('assets/icon.ico')
+frame = Frame(root)
+# ---------------------------------
+def copy(txt):
+    root.clipboard_append(txt)
+    
+def getColor():
+    color = askcolor(title='choose your color')
+    top = Toplevel()
+    top.iconbitmap('assets/icon.ico')
+    Label(top, text='RGB: {}'
+          '\n'
+          'HASH: {}'.format(color[0], color[1])).pack()
+    Button(top, text='copy RGB', command=lambda: copy(color[0])).pack()
+    Button(top, text='copy HASH', command=lambda: copy(color[1])).pack()
+    
+Button(frame, text='Select Color', command=getColor).pack()
+frame.pack()
+mainloop()
